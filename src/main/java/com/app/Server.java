739ed665b8b9e88974b2686b2a6224806c114191
Khethelo Nkosi <saneleknkosi@gmail.com>
2023-09-30 12:28:54 +0200

    stack(server): Added basic functions to run server, also added index.html accessor

--- src/main/java/com/app/Server.java
+++ src/main/java/com/app/Server.java
@@ -1,10 +1,35 @@
 package com.app;
 
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
 /**
  * Server
  */
 public class Server {
+
+    private static final String PAGES_DIR = "/public";
+
+    private final Javalin appServer;
+
+    public Server() {
+        appServer = Javalin.create(config -> config.addStaticFiles(PAGES_DIR, Location.CLASSPATH));
+    }
+
+    public void start(int portNumber) {
+        this.appServer.start(portNumber);
+    }
+
+    public void stop() {
+        this.appServer.stop();
+    }
+
+    public int port() {
+        return this.appServer.port();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Server server = new Server();
+        server.start(5000);
     }
 }
